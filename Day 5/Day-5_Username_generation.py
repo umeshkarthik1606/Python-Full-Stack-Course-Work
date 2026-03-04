8f9a8747ba6c6d982c207cfd3fdff57682882017
umesh karthik <126061613+umeshkarthik1606@users.noreply.github.com>
2026-03-04 11:34:29 +0530

    Delete Day 5 directory

--- Day 5/Day-5_Username_generation.py
+++
@@ -1,9 +0,0 @@
-# username generation
-
-name = input("Enter the name: ")
-dob = input("Enter the dob[YYYY-MM-DD]: ")
-
-username = name[:5]+name[-5]+dob[-2:]+dob[2:4]
-
-print(f"Hi {name}!\nYour Usernaame: {username}")
-

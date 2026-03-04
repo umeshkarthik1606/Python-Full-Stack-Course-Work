a05af28a77ea808fc3005d3ee4891ee241a4faae
umesh karthik <126061613+umeshkarthik1606@users.noreply.github.com>
2026-03-04 11:35:05 +0530

    Delete Day 8 directory

--- Day 8/Day-8_MCQ_test.py
+++
@@ -1,8 +0,0 @@
-data = {
-
-    
-    ["Which keyword is used to define a function in Python?"," function","def","func","define","B"]
-    ["Which of the following is mutable?","tuple","string","list","int","C"]
-        }
-for i in data:
-    print(i)
